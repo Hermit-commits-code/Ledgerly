e88759158e97141b070fe6911cae08b7c0fdccbf
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-04-28 16:00:12 -0400

    chore: fix folder structure, move TabNavigator to navigators/

--- screens/BudgetScreen.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function BudgetScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Budget Screen</Text>
-    </View>
-  );
-}
